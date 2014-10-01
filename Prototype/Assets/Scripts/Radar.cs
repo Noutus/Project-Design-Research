493430e3e9841e0cd01c499452881b1c4d5ea1d9
Arnout Verburg <arnout_verburg@hotmail.com>
2014-10-01 10:59:55 +0200

    Test

---
+++ Prototype/Assets/Scripts/Radar.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//the Radar GameObject represent the two collisions points which go up  
+public class Radar : MonoBehaviour {
+
+	
+	public GameObject SoundPrefab;
+	public int maxNumberPoints;
+	private GameObject[maxNumberPoints] SoundVector;
+
+
+	// Use this for initialization
+	void Start () {
+		maxNumberPoints = 6;
+		SoundVector=(GameObject) GameObject.Instantiate(SoundPrefab);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//this sentence make the two collision points(radar GameObject) go up in the Y axis
+		transform.position = transform.position + Vector3.up * 0.05f;
+
+
+		RaycastHit2D[] vector = Physics2D.CircleCastAll (transform.position, 4f, Vector3.up);
+
+		if (vector.Length != 0) {
+			 foreach(RaycastHit2D e in vector){
+				Debug.Log ( " Hit in position: " + e.point);
+				}
+	}
+}
+}
