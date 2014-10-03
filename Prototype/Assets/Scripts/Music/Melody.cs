641ab641a062b29886dd35c5d9e453519274ba62
Arnout Verburg <arnout_verburg@hotmail.com>
2014-10-03 12:09:51 +0200

    Music Center Line
    Features:
    - Music line in middle of the racing track

---
+++ Prototype/Assets/Scripts/Music/Melody.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Melody : MonoBehaviour
+{
+	public GameObject melodyPrefab;
+
+	private GameObject melody;
+
+	private static int maxIterations = 32;
+
+	void Start()
+	{
+		melody = (GameObject) GameObject.Instantiate(melodyPrefab);
+	}
+	
+	void Update()
+	{
+		Vector2 direction = Vector2.up;
+		RaycastHit2D hitFinal = new RaycastHit2D();
+
+		for (int i = 0; i < maxIterations; i++)
+		{
+			direction = Vector2Helper.Rotate(direction, 360 / maxIterations);
+
+			RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, 10, 1<<8);
+
+			if (hit.collider != null)
+			{
+				if (i == 0)
+				{
+					hitFinal = hit;
+				}
+
+				else 
+				{
+					if (Vector3.Distance(transform.position, hit.point) < Vector3.Distance(transform.position, hitFinal.point))
+				    {
+						hitFinal = hit;
+					}
+				}
+			}
+		}
+
+		melody.transform.position = hitFinal.point;
+	}
+}
